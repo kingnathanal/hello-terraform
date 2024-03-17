e9e679f7c36f1629558f1a5c979a9dcfe9d495f6
kingnathanal <kingnathanal@gmail.com>
2024-03-17 09:07:44 -0400

    trying this again

--- azure/main.tf
+++ azure/main.tf
@@ -40,9 +40,9 @@ resource "azurerm_subnet" "app_subnet" {
 }
 
 module "cheap_azure_kubernetes" {
-  #source  = "app.terraform.io/hyyercode/cheap_aks/azurerm"
-  #version = "1.0.0"
-  source              = "./cheap_azure_kubernetes"
+  source  = "app.terraform.io/hyyercode/cheap_aks/azurerm"
+  version = "1.0.0"
+  #source              = "./cheap_azure_kubernetes"
   resource_group_name = azurerm_resource_group.this.name
   location            = local.location
   vnet_subnet_id      = azurerm_subnet.aks_subnet.id
