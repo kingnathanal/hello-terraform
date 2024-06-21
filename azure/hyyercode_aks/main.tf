0102c28629df12101ca3278154d1324617e34460
kingnathanal <kingnathanal@gmail.com>
2024-06-20 23:34:23 -0400

    format

--- azure/hyyercode_aks/main.tf
+++ azure/hyyercode_aks/main.tf
@@ -55,8 +55,8 @@ module "cheap_azure_kubernetes" {
   aks_dns_prefix      = "hyyercode"
   aks_sku             = "Standard_A2_v2"
   msi_identity = {
-    client_id = azurerm_user_assigned_identity.aks_identity.client_id
-    object_id = azurerm_user_assigned_identity.aks_identity.principal_id
+    client_id        = azurerm_user_assigned_identity.aks_identity.client_id
+    object_id        = azurerm_user_assigned_identity.aks_identity.principal_id
     user_assigned_id = azurerm_user_assigned_identity.aks_identity.id
   }
   identity = {
